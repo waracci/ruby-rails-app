05607d95ae1b08d280d4366f095c0eb452d5c1fb
warachi <morriswarachi0@gmail.com>
2019-03-12 15:51:57 +0300

    - Create a signup.scss and add CSS to the sign up page - Import all files from partials directory to the application.scss

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
   end
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
   root to: 'pages#index'
 end
